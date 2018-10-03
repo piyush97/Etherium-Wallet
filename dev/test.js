fd6ed8b86e586a594998b2e8cb884fc1bdb499de
piyush mehta <me@piyushmehta.com>
2018-10-03 19:30:08 +0530

    block
    Signed-off-by: piyush mehta <me@piyushmehta.com>

--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,6 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
+bitcoin.createNewBlock(2389, '0INASDASJD123', '123SAOIQ2');
 /* eslint-disable-next-line no-console */
 console.log(bitcoin);
